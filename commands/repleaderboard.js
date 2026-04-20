7cc50b2957b529d0a0237d656522b473c3d012b4
YoDoesDev <hellowspaa@gmail.com>
2026-04-20 12:52:20 +0530

    Update repleaderboard.js
    Finished a part of writing the file

--- commands/repleaderboard.js
+++ commands/repleaderboard.js
@@ -1,1 +1,8 @@
+const { db } = require('../database.js');
 
+module.exports = {
+  name: 'repleaderboard', 
+  aliases: 'replb', 'rl', 
+  description: 'Shows a leaderboard of members with highest reputation points. ',
+  async execute(message) 
+}
