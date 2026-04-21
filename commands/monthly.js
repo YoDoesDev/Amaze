36dcd33e42f6b4609d5ac70aafe04c46c2f3552b
YoDoesDev <hellowspaa@gmail.com>
2026-04-21 18:43:43 +0530

    Add files via upload

---
+++ commands/monthly.js
@@ -0,0 +1,40 @@
+const { EmbedBuilder } = require('discord.js');
+const { db } = require('../database.js');
+
+module.exports = {
+  name: 'monthly', 
+  category: 'Economy', 
+  description: 'Use this command to gain amash every month.', 
+  async execute(message){
+    const embed = new EmbedBuilder()
+      .setTitle("Amash collected!")
+      .setDescription("You receive 400 Amash! Come back next month!")
+      .setColor('#57F287');
+    
+    db.get(`SELECT mTimestamp FROM amash WHERE userid = ?`, [message.author.id], (err, row) => {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      
+      const now = Date.now();
+      const cooldown = 2592000000;
+      
+      if(row && (now - row.mTimestamp < cooldown)){
+        const days = Math.floor((cooldown - (now - row.mTimestamp))/86400000);
+        const hrs = Math.floor((cooldown - (now - row.mTimestamp))/3600000);
+        return message.reply("Be patient! You can claim your monthly in " + days > 1? (days + "days."):(hrs + " hours."));
+      }  
+
+      db.run(`INSERT INTO amash (userid, bucks, mTimestamp) 
+      VALUES (?, ?, ?)
+      ON CONFLICT (userid) 
+      DO UPDATE SET bucks = bucks + 400,
+      mTimestamp = excluded.mTimestamp
+      `, [message.author.id, 400, now], (err) => {
+          if (err) return console.log(err);
+          message.reply({ embeds: [embed] });
+      });
+    });
+  }
+}
