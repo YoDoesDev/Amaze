42d9b4c555902f188bdc3d46be1446bb17d2810e
YoDoesDev <hellowspaa@gmail.com>
2026-04-24 16:12:57 +0530

    Create portfolio.js
    Portfolio command shows the details of stocks they have bought.

---
+++ commands/portfolio.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require('discord.js');
+const { db } = require('../database.js');
+
+module.exports = {
+  name: 'portfolio', 
+  category: 'Economy', 
+  description: 'Shows a list of stocks you have bought', 
+  async execute(message){
+    const target = message.mentions.users.first() || message.author;
+    db.all(`SELECT invested, stocks, profit FROM investments WHERE investor = ?`, [target.id], (err, rows) => {
+      if (err){
+        console.log(err);
+        return message.reply("A Database Error Occured!");
+      }
+      if(rows.length == 0){
+        return message.reply("You have not bought any stocks yet!");
+      }
+      
+      const list = rows.map((row, index) => {
+        return `**${index + 1}.** Invested in <@${row.invested}>: **${row.stocks}** stocks (${row.profit} profit)`;
+    }).join('\n');
+      
+      let embed = new EmbedBuilder().setColor('#E8AE1B').setTitle(`${target.username}'s portfolio`).setDescription(list).setFooter({text: `Requested By: ${message.author.username}.`}).setTimestamp();
+      
+      return message.reply({embeds: [embed]})
+    })
+  }
+}
