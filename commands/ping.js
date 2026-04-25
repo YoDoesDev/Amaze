be1c7200c3bece86d1ef7d8e0c6dabb514f78a31
YoDoesDev <hellowspaa@gmail.com>
2026-04-25 19:52:54 +0530

    Update ping.js
    Removed slash command (old commit had wrong code)

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,11 @@
 module.exports = {
     name: 'ping',
     description: 'Check the bot latency',
-    async execute(context) { 
-        const isSlash = context.isChatInputCommand && context.isChatInputCommand();
-    
-        const latency = Date.now() - context.createdTimestamp;
-        const heartbeat = context.client.ws.ping;
+    execute(message) {
+        const latency = Date.now() - message.createdTimestamp;
+        
+        const heartbeat = message.client.ws.ping;
 
-        const response = `🏓 **Pong!**\n**Latency:** ${latency}ms\n**API Heartbeat:** ${heartbeat}ms`;
-
-        if (isSlash) {
-            await context.reply({ content: response });
-        } else {
-            await context.reply(response);
-        }
+        message.reply(`🏓 **Pong!**\n**Latency:** ${latency}ms\n**API Heartbeat:** ${heartbeat}ms`);
     },
 };
