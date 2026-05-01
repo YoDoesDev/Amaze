7dff9172a732a491370a96118b11f27be3640432
YoDoesDev <hellowspaa@gmail.com>
2026-05-01 16:05:33 +0530

    Update index.js
    SFTP deployment check

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Automated Deploy Active: May 1, 2026.
 const { Client, Collection, GatewayIntentBits, REST, Routes } = require('discord.js');
 const { initDb, db } = require('./database.js');
 const fs = require('fs');
