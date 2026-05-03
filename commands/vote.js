80f6d7644ef19b97093d2fdd84cb7fad770dd14b
YoDoesDev <hellowspaa@gmail.com>
2026-05-03 23:09:09 +0530

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,12 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'vote', 
+  aliases: ['v'], 
+  description: 'Use this command to vote our bot on top.gg!', 
+  async execute(message){
+    const embed = new EmbedBuilder().setTitle("Vote Us!").setDescription("Glad that you want to vote me!\n\nVote me on: https://top.gg/bot/1494637741722566656/vote");
+    
+    return message.reply({embeds: [embed]})
+  }
+}
