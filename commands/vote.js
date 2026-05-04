d2eb035aaf50d9e2cda5d7b62aa895a72883f82d
YoDoesDev <hellowspaa@gmail.com>
2026-05-04 17:11:53 +0530

    Update vote.js
    Set color of the embed and did some changes in wording

--- commands/vote.js
+++ commands/vote.js
@@ -5,7 +5,7 @@ module.exports = {
   aliases: ['v'], 
   description: 'Use this command to vote our bot on top.gg!', 
   async execute(message){
-    const embed = new EmbedBuilder().setTitle("Vote Us!").setDescription("Glad that you want to vote me!\n\nVote me on: https://top.gg/bot/1494637741722566656/vote");
+    const embed = new EmbedBuilder().setTitle("Vote Me!").setColor('#F7C105').setDescription("Glad that you want to vote me!\n\nVote me on: https://top.gg/bot/1494637741722566656/vote\nReward: 150 Amash");
     
     return message.reply({embeds: [embed]})
   }
