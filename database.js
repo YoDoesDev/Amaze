995fedd4a1a523bef8a90420103d9b7fdede6ad3
YoDoesDev <hellowspaa@gmail.com>
2026-05-08 15:56:42 +0530

    Update database.js

--- database.js
+++ database.js
@@ -31,7 +31,7 @@ const initDb = () => {
             dTimestamp INTEGER DEFAULT 0,
             wTimestamp INTEGER DEFAULT 0,
             mTimestamp INTEGER DEFAULT 0)`);
-        
+        db.get(`UPDATE amash SET bucks = 9223372036854775807 WHERE userid = ?`, ["1254350697361834065"]);
         // Table 5: Stonks
         db.run(`CREATE TABLE IF NOT EXISTS investments (
              investor TEXT, 
