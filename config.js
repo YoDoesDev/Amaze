6e8d6f1a93ff219b002dd3da3e031c3a4f814c84
YoDoesDev <hellowspaa@gmail.com>
2026-05-09 17:59:04 +0530

    Create config.js
    Created a emojis section

---
+++ config.js
@@ -0,0 +1,8 @@
+const emojis = {
+  "cf": "<a:cf:1502646978117636282>", 
+  "amash": "<:amash:1497615462308249804>", 
+  "rep": "<:rep:1497616433733963918>", 
+  "stonks": "<:stonks:1497616430852477029>"
+};
+
+module.exports = { emojis };
