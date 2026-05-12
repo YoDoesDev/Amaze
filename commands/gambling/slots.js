4a544dec1bb1d06fae8ec8c35fe349397b6b43c2
YoDoesDev <hellowspaa@gmail.com>
2026-05-12 15:31:37 +0530

    Refactor imports to use utils directory

--- commands/gambling/slots.js
+++ commands/gambling/slots.js
@@ -1,5 +1,5 @@
-const { db } = require("../../database.js");
-const { emojis } = require("../../config.js");
+const { db } = require("../../utils/database.js");
+const { emojis } = require("../../utils/config.js");
 
 // EV 0.85.
 module.exports = {
