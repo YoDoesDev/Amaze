21a378122dcbed9f321dbe144dbfb14fca9849ea
YoDoesDev <hellowspaa@gmail.com>
2026-05-14 17:30:11 +0530

    Add dotenv configuration to slash-deploy.js
    Load environment variables from .env file.

--- utils/slash-deploy.js
+++ utils/slash-deploy.js
@@ -1,6 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 // Import your config/token
 const { clientId, guildId } = require('./config.js'); 
 
