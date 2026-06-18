4471db80e71aa112102804cefe67ae4954be6a30
YoDoesDev <hellowspaa@gmail.com>
2026-06-18 15:50:15 +0530

    update slay.js

---
+++ slashCommands/slay/slay.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
+
+const data = new SlashCommandBuilder()
+    .setName('slay')
+    .setDescription('Main entry point for the RPG game')
+    .addSubcommand(sub => 
+        sub.setName('charinfo').setDescription('View your character information')
+    )
+    .addSubcommand(sub => 
+        sub.setName('hunt').setDescription('Go hunting for loot')
+    );
+
+module.exports = {
+    data: data,
+    async execute(interaction) {
+        // Safe check: if for some reason it wasn't deferred, defer it now
+        if (!interaction.deferred) await interaction.deferReply();
+
+        const subcommandName = interaction.options.getSubcommand();
+
+        try {
+            const subcommandPath = path.join(__dirname, 'subs', `${subcommandName}.js`);
+            
+            if (!fs.existsSync(subcommandPath)) {
+                return await interaction.editReply({ 
+                    content: `The sub-command \`${subcommandName}\` file could not be found.`
+                });
+            }
+
+            const subcommand = require(subcommandPath);
+            // Pass the interaction along to the sub-command file
+            await subcommand.execute(interaction);
+        } catch (err) {
+            console.error("Slash RPG Error:", err);
+            await interaction.followUp({ content: 'There was an error executing this action.', ephemeral: true });
+        }
+    }
+};
